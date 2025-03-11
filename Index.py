b78873c292ac422a69d2d3450e7509294be13afe
AKK-96 <kariukikimuhu@gmail.com>
2025-03-11 11:45:32 +0300

    Initial commit

--- Index.py
+++ Index.py
@@ -1,1 +1,15 @@
 print("Hello world")
+
+number = 152846464
+print (number)
+
+
+
+age = 27 # Int DataType
+marks = 90.5 # Float DataType
+
+name = "Motokoooooo" # String DataType
+
+languages = ["python", "HTML", "CSS", 3, 5.7, True]
+print (languages[-3])
+
